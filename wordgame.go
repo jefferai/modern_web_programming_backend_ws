4da3e9cdade90e043ee830a41ba35f8e4e113572
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2013-05-28 11:18:31 -0400

    Add code, including markup but excluding solution

---
+++ wordgame.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"crypto/rand"
+	"io"
+	"math"
+	"math/big"
+	"strings"
+)
+
+type Game struct {
+	NumGuesses int64
+	Word       string
+	MaskedWord string
+}
+
+var (
+	games          = map[int64]Game{}
+	words          = []string{"aardvark", "labradoodle", "kittycat", "porpoise", "brontosaurus"}
+	underscoreFunc = func(r rune) rune { return '_' }
+)
+
+// This ignores safety for simplicity
+func UnhideByte(guess byte, word string, maskedword string) string {
+	wordbytes := []byte(word)
+	maskedwordbytes := []byte(maskedword)
+	for pos := range wordbytes {
+		if wordbytes[pos] == guess {
+			maskedwordbytes[pos] = wordbytes[pos]
+		}
+	}
+	return string(maskedwordbytes)
+}
+
+// STARTWGMNG OMIT
+func (wg *wordgame) makeNewGame() int64 {
+
+	// skipping some error handling for presentation reasons
+	randint, _ := rand.Int(rand.Reader, big.NewInt(int64(math.MaxUint32)))
+
+	currgame := randint.Int64()
+	wordnum := currgame % int64(len(words))
+
+	games[currgame] = Game{
+		NumGuesses: 0,
+		Word:       words[wordnum],
+		MaskedWord: strings.Map(underscoreFunc, words[wordnum]),
+	}
+
+	wg.send <- []byte("Hello, welcome to the game!\n")
+	return currgame
+}
+
+func (wg *wordgame) removeGame() {
+	delete(games, wg.gameId)
+}
+// ENDWGMNG OMIT
+
+func (wg *wordgame) processMessage(reader io.Reader) {
+	return
+}
---
+++ wshandlers.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"github.com/garyburd/go-websocket/websocket"
+	"log"
+	"time"
+)
+
+// STARTWGINIT OMIT
+type wordgame struct {
+	ws     *websocket.Conn
+	send   chan []byte
+	finish bool //not protecting with a mutex, but you should
+	gameId int64
+}
+
+func NewWordGame(w *websocket.Conn) (ret *wordgame) {
+	wg := new(wordgame)
+	wg.ws = w
+	wg.send = make(chan []byte, 10)
+	wg.finish = false
+	wg.gameId = wg.makeNewGame()
+	return wg
+}
+
+func (wg *wordgame) Cleanup() {
+	log.Println("Cleaning up word game with id", wg.gameId)
+	wg.removeGame()
+	close(wg.send)
+	wg.ws.Close()
+}
+
+// ENDWGINIT OMIT
+
+// STARTRP OMIT
+func (wg *wordgame) ReadPump() {
+	for {
+		switch wg.finish {
+		case true:
+			return
+		default:
+			// Use deadline to detect dead or stuck clients.
+			wg.ws.SetReadDeadline(time.Now().Add(300 * time.Second))
+			op, reader, err := wg.ws.NextReader() //blocks
+			if err != nil {
+				log.Println("Error getting next reader: ", err)
+				return
+			}
+			if op == websocket.OpClose ||
+				op == websocket.OpBinary { //binary messages are not expected
+				return // fall through to cleanup
+			}
+			if op == websocket.OpText {
+				wg.processMessage(reader)
+			}
+			// ignore pongs, pings, and other types and cycle around
+		}
+	}
+}
+
+// ENDRP OMIT
+
+// STARTWP OMIT
+func (wg *wordgame) WritePump() {
+	ticker := time.NewTicker(4 * time.Second)
+	defer ticker.Stop()
+	for {
+		if wg.finish {
+			return
+		}
+		select {
+		case message, ok := <-wg.send:
+			if !ok {
+				_ = wg.write(websocket.OpClose, []byte{}) // don't care about error, closing
+				wg.finish = true
+			} else if string(message) == "__MAGIC_CLOSE_VALUE__" {
+				wg.finish = true
+			} else if err := wg.write(websocket.OpText, message); err != nil {
+				wg.finish = true
+			}
+		case <-ticker.C:
+			if err := wg.write(websocket.OpPing, []byte{}); err != nil {
+				wg.finish = true
+			}
+		}
+	}
+}
+
+// ENDWP OMIT
+
+// STARTWRITE OMIT
+func (wg *wordgame) write(opCode int, payload []byte) error {
+	wg.ws.SetWriteDeadline(time.Now().Add(30 * time.Second))
+	w, err := wg.ws.NextWriter(opCode)
+	if err != nil {
+		return err
+	}
+	if _, err := w.Write(payload); err != nil {
+		w.Close()
+		return err
+	}
+	return w.Close()
+}
+
+// ENDWRITE OMIT
